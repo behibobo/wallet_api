a50c5d7a64af17f126aa30a50a5a3ffe0dcc55de
behzad <behzadfareed@gmail.com>
2020-01-13 19:49:20 +0330

    sss

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :username, :full_name
+  attributes :id, :first_name, :last_name, :username, :full_name, :credit
 
   def full_name
     object.full_name
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   post 'auth/login', to: 'auth#login'
   post 'auth/signup', to: 'auth#signup'
+  get 'dashboard', to: 'dashboard#index'
 end
